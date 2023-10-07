cae4317d1e6f3dc2106055c3b8c50137840e927b
ximenalopezm <ximenaooflopez@gmail.com>
2023-10-07 08:15:23 -0700

    Degradado tentativo

---
+++ star/src/pages/LogIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/LogIn.css';
+
+function LogIn() {
+    return (
+      <div className="gradient-background">
+
+      </div>
+    );
+}
+
+export default LogIn;
