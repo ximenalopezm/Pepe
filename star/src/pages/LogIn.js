8769f960200a2d13a425383e998ef749f39640bb
NovoxMX <A00835132@tec.mx>
2023-10-07 12:36:33 -0700

    add text

--- star/src/pages/LogIn.js
+++ star/src/pages/LogIn.js
@@ -8,9 +8,12 @@ function LogIn() {
         <div className="login-container">
           <div className="login-form">
             <img src={logo} alt="logo" />
-            
+            <div className="text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
             <div id = "uploader" className='uploader'>
               <button className="btn btn-primary btn-block btn-lg" type="submit">Select PDF file</button>
+              <p>or drag and drop files here</p>
             </div>
           </div>
         </div>
