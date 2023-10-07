9654ff47600adc85200b60b06377dd8bd47e68f1
ximenalopezm <ximenaooflopez@gmail.com>
2023-10-07 08:16:35 -0700

    Mensajito

--- star/src/App.js
+++ star/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import LogIn from '../src/pages/LogIn';
+
+// Chiques puse esto en la app para ver como se veia e irle moviendo para hacer el fondo del login
 
 function App() {
   return (
