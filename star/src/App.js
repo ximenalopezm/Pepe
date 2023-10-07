cd857a48c6444e87c11e2b44ca83f941c978da1d
NovoxMX <A00835132@tec.mx>
2023-10-07 09:08:05 -0700

    Logo and change background color

--- star/src/App.js
+++ star/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import LogIn from '../src/pages/LogIn';
+import LogIn from './pages/login';
 
 // Chiques puse esto en la app para ver como se veia e irle moviendo para hacer el fondo del login
 
--- star/src/pages/LogIn.js
+++ star/src/pages/LogIn.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import '../styles/LogIn.css';
+import logo from "../images/Logo.png";
 
 function LogIn() {
     return (
       <div className="gradient-background">
-
+        <div className="login-container">
+          <div className="login-form">
+            <img src={logo} alt="logo" />
+            
+            <div id = "uploader" className='uploader'>
+              
+            </div>
+          </div>
+        </div>
       </div>
     );
 }
