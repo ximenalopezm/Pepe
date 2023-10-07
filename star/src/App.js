2015e66ef2ab28402a2ab77642cf5fd76cb8ee92
ximenalopezm <ximenaooflopez@gmail.com>
2023-10-07 11:39:38 -0700

    name

--- star/src/App.js
+++ star/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import LogIn from './pages/login';
+import LogIn from './pages/LogIn';
 
 // Chiques puse esto en la app para ver como se veia e irle moviendo para hacer el fondo del login
 
